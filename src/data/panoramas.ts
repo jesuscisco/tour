432b1a037815716edd1333ac741d8439a43359b8
jesuscisco <jesus.ciscomaniv@gmail.com>
2025-09-30 14:00:54 -0700

    rename imgs2

--- src/data/panoramas.ts
+++ src/data/panoramas.ts
@@ -7,7 +7,7 @@ export type PanoramaMeta = {
 const PANORAMA_META: Record<string, PanoramaMeta> = {
   '/panoramas/INICIO.png': { initialLon: 0, initialLat: 0, radius: 480 },
   '/panoramas/1.png':    { initialLon: -100, initialLat: 10, radius: 480 },
-  '/panoramas/2.png':    { initialLon: 0, initialLat: 2, radius: 480 },
+  '/panoramas/20.png':    { initialLon: 0, initialLat: 2, radius: 480 },
   '/panoramas/2b.png':   { initialLon: -30, initialLat: 2, radius: 480 },
   '/panoramas/3.png':    { initialLon: -30, initialLat: 2, radius: 480 },
   '/panoramas/4.png':    { initialLon: -30, initialLat: 2, radius: 480 },
