918823ccfe0b11f8e24247e40ef5d1707944efe5
jesuscisco <jesus.ciscomaniv@gmail.com>
2025-10-21 04:09:34 -0700

    5bmodificada5

--- src/data/hotspots.ts
+++ src/data/hotspots.ts
@@ -15,7 +15,7 @@ const HOTSPOTS_MAP: Record<string, HotspotData[]> = {
   ],
   '/panoramas/2.png': [
     { id: 'to-1', position: [-200, 7, -35], label: 'ir a 1', target: '/panoramas/1.png' },
-    { id: 'to-3b', position: [-95, 0, -900], label: 'ir a 3b', target: '/panoramas/3b.webp' },
+    { id: 'to-3b', position: [-95, 0, -900], label: 'ir a 3b', target: '/panoramas/3b.png' },
     { id: 'to-3', position: [80, 0, 5], label: 'ir a 3', target: '/panoramas/3.png' },
   ],
   '/panoramas/3.png': [
@@ -30,13 +30,13 @@ const HOTSPOTS_MAP: Record<string, HotspotData[]> = {
     { id: 'to-4-again', position: [-80, -5, -3], label: 'Ir a 4', target: '/panoramas/4.png' },
   ],
 
-  '/panoramas/3b.webp': [
+  '/panoramas/3b.png': [
     { id: 'to-2-again', position: [400, -170, -120], label: 'Ir a 2', target: '/panoramas/2.png' },
-    { id: 'to-11', position: [400, 220, 100], label: 'Ir a 11', target: '/panoramas/11.png' },
+    { id: 'to-5b', position: [400, 220, 100], label: 'Ir a 5b', target: '/panoramas/5b.png' },
   ],
 
-  '/panoramas/11.png': [
-    { id: 'to-3b-again', position: [100, -130, 800], label: 'Ir a 3b', target: '/panoramas/3b.webp' },
+  '/panoramas/5b.png': [
+    { id: 'to-3b-again', position: [100, -130, 800], label: 'Ir a 3b', target: '/panoramas/3b.png' },
     { id: 'to-6b', position: [-100, -20, 30], label: 'Ir a 6b', target: '/panoramas/6b.png' },
     { id: 'to-7b', position: [-100, -20, -80], label: 'Ir a 7b', target: '/panoramas/7b.png' },
     { id: 'to-8b', position: [-60, -130, -900], label: 'Ir a 8b', target: '/panoramas/8b.png' },
@@ -44,19 +44,19 @@ const HOTSPOTS_MAP: Record<string, HotspotData[]> = {
   ],
 
   '/panoramas/6b.png': [
-    { id: 'to-11-again', position: [-60, -130, -900], label: 'Ir a 11', target: '/panoramas/11.png' },
+    { id: 'to-5b-again', position: [-60, -130, -900], label: 'Ir a 5b', target: '/panoramas/5b.png' },
   ],
 
 '/panoramas/7b.png': [
-    { id: 'to-11-again', position:[-40, 0, 30], label: 'Ir a 11', target: '/panoramas/11.png' },
+    { id: 'to-5b-again', position:[-40, 0, 30], label: 'Ir a 5b', target: '/panoramas/5b.png' },
   ],
 
   '/panoramas/8b.png': [
-    { id: 'to-11-again', position: [-100, 0, 60], label: 'Ir a 11', target: '/panoramas/11.png' },
+    { id: 'to-5b-again', position: [-100, 0, 60], label: 'Ir a 5b', target: '/panoramas/5b.png' },
   ],
 
   '/panoramas/9b.png': [
-    { id: 'to-11-again', position: [-700, -100, -400], label: 'Ir a 11', target: '/panoramas/11.png' },
+    { id: 'to-5b-again', position: [-700, -100, -400], label: 'Ir a 5b', target: '/panoramas/5b.png' },
     { id: 'to-10', position: [-300, -40, 60], label: 'Ir a 10', target: '/panoramas/10.png' },
   ],
 
@@ -64,7 +64,6 @@ const HOTSPOTS_MAP: Record<string, HotspotData[]> = {
     { id: 'to-9b-again', position: [-300, 0, -400], label: 'Ir a 1', target: '/panoramas/9b.png' },
   ],
 };
-
 
 
 
